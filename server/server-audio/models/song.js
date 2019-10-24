502acc18db554385a41c4c04d601feed83307498
Khuong Dang <khuongdangxuanduy@gmail.com>
2019-10-24 14:20:20 +0700

    *Delete some leftover

--- server/server-audio/models/song.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-    url : String,
-    name : String,
-    artist : String
-});
-
-module.exports = mongoose.model('songs',schema);
