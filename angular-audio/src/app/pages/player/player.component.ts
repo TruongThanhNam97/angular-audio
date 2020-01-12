5da59cc330bfa41e6f23fc72e61ba5165c42210a
TruongThanhNam97 <15520526@gm.uit.edu.vn>
2020-01-12 10:00:00 +0700

    Deleted residual code

--- angular-audio/src/app/pages/player/player.component.ts
+++ angular-audio/src/app/pages/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AudioService } from '../../services/audio.service';
 import { CloudService } from '../../services/cloud.service';
 import { StreamState } from '../../interfaces/stream-state';
